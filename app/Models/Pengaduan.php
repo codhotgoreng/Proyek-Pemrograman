881ae6f1dc9202680c0357f3f37ce16e48269fb6
Codhot <naafi343@gmail.com>
2025-06-24 05:40:10 +0700

    fix pengaduan

--- app/Models/Pengaduan.php
+++ app/Models/Pengaduan.php
@@ -8,8 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Pengaduan extends Model
 {
     use HasFactory;
+
     protected $guarded = ['id_pengaduan'];
     protected $table = 'pengaduan';
+
+    // Tambahkan ini:
+    protected $primaryKey = 'id_pengaduan';
+    public $incrementing = true; // optional, tapi baik jika eksplisit
+    protected $keyType = 'int'; // karena kolomnya tipe integer
+
     public function masyarakat()
     {
         return $this->belongsTo(Masyarakat::class, 'nik', 'nik');
